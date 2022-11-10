77f0d06da24a3a5a299b5686717cc056a69d913a
Geraldo Mendes <92949630+Geraldomendes@users.noreply.github.com>
2022-11-09 23:34:40 -0300

    Adicinonando questão CBBAtividadePratica 2 e 3

--- CBBAtividadePratica3.java
+++ CBBAtividadePratica3.java
@@ -1,9 +1,9 @@
-public class CBBAtividadePratica2 {
+public class CBBAtividadePratica3 {
 
 	public static void main(String[] args) {
 		int[] num = new int [100];
-		for (int i=0; i<100; i++) {
-			num[i]=i+1;
+		for (int i=0; i<num.length; i++) {
+			num[i]=i;
 			System.out.println(num[i]);
 		}
 	}
